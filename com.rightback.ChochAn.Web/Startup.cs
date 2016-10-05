ae45073570310a8f0a81e084baf9500d27e51e75
Beri Bener <beribener@hotmail.com>
2016-10-05 19:35:32 -0400

    Added mvc web project

---
+++ com.rightback.ChochAn.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(com.rightback.ChochAn.Web.Startup))]
+namespace com.rightback.ChochAn.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
