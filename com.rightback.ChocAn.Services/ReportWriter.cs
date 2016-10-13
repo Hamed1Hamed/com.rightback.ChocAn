54bf1a61ca13728af718e0ad481a93e3d07b48a3
maged <maged@MAGED-PC>
2016-10-13 14:51:22 -0500

    utility class for writing data tables into html or csv file

---
+++ com.rightback.ChocAn.Services/ReportWriter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.rightback.ChocAn.Services
+{
+    public static class ReportWriter
+    {
+        public static void CreateCSVFile(DataTable dt, string strFilePath)
+        {
+            try
+            {
+                // Create the CSV file to which grid data will be exported.
+                StreamWriter sw = new StreamWriter(strFilePath, false);
+                // First we will write the headers.
+                int iColCount = dt.Columns.Count;
+                for (int i = 0; i < iColCount; i++)
+                {
+                    sw.Write(dt.Columns[i]);
+                    if (i < iColCount - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+
+                // Now write all the rows.
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    for (int i = 0; i < iColCount; i++)
+                    {
+                        if (!Convert.IsDBNull(dr[i]))
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                        if (i < iColCount - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+
+                    sw.Write(sw.NewLine);
+                }
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static  void CreateHtmlFile(DataTable dt, string strFilePath)
+        {
+            string content = ConvertDataTableToHTML(dt);
+            try
+            {
+                // Use FileIO to replace the content of the text file
+                StreamWriter sw = new StreamWriter(strFilePath, false);
+                sw.Write(content);
+                sw.Close();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex; ;
+            }
+        }
+
+        private static string ConvertDataTableToHTML(DataTable dt)
+    {
+        string html = "<table>";
+        //add header row
+        html += "<tr>";
+        for (int i = 0; i < dt.Columns.Count; i++)
+            html += "<td>" + dt.Columns[i].ColumnName + "</td>";
+        html += "</tr>";
+        //add rows
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            html += "<tr>";
+            for (int j = 0; j < dt.Columns.Count; j++)
+                html += "<td>" + dt.Rows[i][j].ToString() + "</td>";
+            html += "</tr>";
+        }
+        html += "</table>";
+        return html;
+    }
+}
+    }
