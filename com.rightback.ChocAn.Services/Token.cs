e99b5dd7cf203bbc3c750af298264836e21677f4
maged <maged@MAGED-PC>
2016-10-14 17:27:09 -0500

    two helper classes we may need Token generation,string to int

---
+++ com.rightback.ChocAn.Services/Token.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.rightback.ChocAn.Services
+{
+    class Token
+    {
+
+        private static readonly Object obj = new Object();
+        private static readonly Random random = new Random();
+        public static string CreateShortUniqueString()
+        {
+            string strDate = DateTime.Now.ToString("yyyyMMddhhmmssfff");
+            string randomString;
+            lock (obj)
+            {
+                randomString = RandomString(3);
+            }
+            return strDate + randomString; // 16 charater
+        }
+        private static string RandomString(int length)
+        {
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxy";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+    }
+}
+}
