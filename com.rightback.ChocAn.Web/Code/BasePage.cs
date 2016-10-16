788a1e568916fb9ef73a3d32571f461619b4579b
Beri Bener <beribener@hotmail.com>
2016-10-16 00:39:53 -0400

    Class diagram, minor folder organizations

--- com.rightback.ChocAn.Web/Code/BasePage.cs
+++ com.rightback.ChocAn.Web/Code/BasePage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using com.rightback.ChocAn.Services.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,6 @@ namespace com.rightback.ChocAn.Web.Code
 {
     public class BasePage : System.Web.UI.Page
     {
+      
     }
 }
