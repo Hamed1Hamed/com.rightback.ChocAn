eb75553cae1d8fdbb03adbaa676259430ad88b42
maged <maged@MAGED-PC>
2016-11-24 00:20:19 -0600

    delete unused classes

---
+++ com.rightback.ChocAn.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.rightback.ChocAn.Services.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.rightback.ChocAn.Services.Extensions.Tests
+{
+    [TestClass()]
+    public class DateTimeExtensionsTests
+    {
+        [TestMethod()]
+        public void NextTest()
+        {
+            //saturday
+            DateTime date = new DateTime(2016, 11, 19);
+            //next monday should be 11/21/2016
+           DateTime nextMonday = date.Next(DayOfWeek.Monday);
+
+            Assert.AreEqual(date.Day, 21);
+            Assert.AreEqual(date.Month, 11);
+            Assert.AreEqual(date.Year, 2016);
+        }
+
+        [TestMethod()]
+        public void PreviousTest()
+        {  //saturday
+            DateTime date = new DateTime(2016, 11, 19);
+            //previos monday should be 11/14/2016
+            DateTime nextMonday = date.Next(DayOfWeek.Monday);
+
+            Assert.AreEqual(date.Day, 14);
+            Assert.AreEqual(date.Month, 11);
+            Assert.AreEqual(date.Year, 2016);
+        }
+    }
+}
---
+++ com.rightback.ChocAn.Tests/Extensions/StateExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.rightback.ChocAn.DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.rightback.ChocAn.DAL.Enums;
+
+namespace com.rightback.ChocAn.DAL.Enums.Tests
+{
+    [TestClass()]
+    public class StateExtensionTests
+    {
+        [TestMethod()]
+        public void GetStateFullNameTest()
+        {
+            string alaska = StateExtension.GetStateFullName(State.AL);
+            Assert.IsTrue("ALASKA".Equals( alaska));
+        }
+
+        [TestMethod()]
+        public void GetStateByNameTest()
+        {
+            State alaska = StateExtension.GetStateByName("ALASKA");
+            Assert.IsTrue(alaska.Equals(State.AL));
+        }
+
+
+    }
+}
